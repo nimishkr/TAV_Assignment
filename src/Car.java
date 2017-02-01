c9809c6a29cb6bd356ab587a6ae1f485e4f36a7e
nimishkr <nimish97@yahoo.co.in>
2017-02-01 23:38:42 +0100

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,35 @@
+/**
+ * Created by Nimish on 28/01/2017.
+ */
+public class Car implements ParkCar{
+
+    @Override
+    public void moveForward() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void moveBackward() {
+
+    }
+
+    @Override
+    public void park() {
+
+    }
+
+    @Override
+    public void unPark() {
+
+    }
+
+    @Override
+    public void whereIs() {
+
+    }
+}
